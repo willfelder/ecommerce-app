337632d0e86c9329175b85e9905dded1420b2f6c
William Lengenfelder <william.lengenfelder@gmail.com>
2021-10-02 21:14:06 -0300

    update app
    Today I added two new components to the app. Newsletter and Footer components.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import styled from 'styled-components';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import RoomIcon from '@mui/icons-material/Room';
+import PhoneIcon from '@mui/icons-material/Phone';
+import EmailIcon from '@mui/icons-material/Email';
+
+const Container = styled.div`
+    display: flex;
+`
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+`
+const Logo = styled.h1`
+    
+`
+const Description = styled.p`
+    margin: 20px 0px;
+`
+const SocialContainer = styled.div`
+    display: flex;
+`
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: #fff;
+    background-color: #${props => props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+`
+const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+`
+const Title = styled.h3`
+    margin-bottom: 30px;
+`
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`
+const ListIcon = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`
+const Right = styled.div`
+    flex: 1;
+    padding: 20px;
+`  
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`  
+const Payment = styled.img`
+    width: 50%;
+`
+
+export default function Footer(){
+
+    return(
+        <Container>
+            <Left>
+                <Logo>PADOVA</Logo>
+                <Description>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, earum doloremque aperiam quas nihil recusandae numquam exercitationem libero repellat alias autem vitae culpa labore eos vero aspernatur? Inventore, deleniti eos!</Description>
+                <SocialContainer>
+                    <SocialIcon color="3B5999">
+                        <FacebookIcon />
+                    </SocialIcon>
+                    <SocialIcon color="E4405F">
+                        <InstagramIcon/>
+                    </SocialIcon>
+                    <SocialIcon color="55ACEE">
+                        <TwitterIcon/>
+                    </SocialIcon>
+                    <SocialIcon color="E60023">
+                        <PinterestIcon/>
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListIcon>Home</ListIcon>
+                    <ListIcon>Cart</ListIcon>
+                    <ListIcon>Man Fashion</ListIcon>
+                    <ListIcon>Woman Fashion</ListIcon>
+                    <ListIcon>Accessories</ListIcon>
+                    <ListIcon>My Account</ListIcon>
+                    <ListIcon>Order Tracking</ListIcon>
+                    <ListIcon>Wishlist</ListIcon>
+                    <ListIcon>Terms</ListIcon>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem>
+                    <RoomIcon style={{ marginRight: "10px" }} /> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </ContactItem>
+                <ContactItem>
+                    <PhoneIcon style={{ marginRight: "10px" }} /> +49 524 548 942
+                </ContactItem>
+                <ContactItem>
+                    <EmailIcon style={{ marginRight: "10px" }} /> contact@gmail.com
+                </ContactItem>
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+            </Right>
+        </Container>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Announcement from '../components/Announcement';
 import Categories from '../components/Categories';
+import Footer from '../components/Footer';
 import Navbar  from '../components/Navbar';
+import NewsLetter from '../components/Newsletter';
 import Products from '../components/Products';
 import Slider from '../components/Slider';
 
@@ -14,6 +16,8 @@ export default function Home(){
           <Slider />
           <Categories />
           <Products />
+          <NewsLetter/>
+          <Footer/>
         </>
     )
 }
