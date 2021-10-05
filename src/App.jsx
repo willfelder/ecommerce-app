aa63c19c26fe9eb89f6b228f42d2d78514500ebe
William Lengenfelder <william.lengenfelder@gmail.com>
2021-10-05 19:17:54 -0300

    update app
    I added new page to the app called Cart

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import ProductList from './pages/ProductList';
 import Product from './pages/Product';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Cart from './pages/Cart';
 
 export default function App() {
   return(
     <>
-      <Login/>
+      <Cart/>
     </>
   )
 }
