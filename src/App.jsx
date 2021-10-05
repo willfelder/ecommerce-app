eb440ff3a9e9fa7247bbfa1b97aae94c7a0b5625
William Lengenfelder <william.lengenfelder@gmail.com>
2021-10-04 21:20:24 -0300

    update app
    I added a new page Product

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
+import Product from './pages/Product';
 
 export default function App() {
   return(
     <>
-      <ProductList/>
+      <Product/>
     </>
   )
 }
---
+++ src/pages/Product.jsx
@@ -0,0 +1,146 @@
+import { Add, Remove } from '@mui/icons-material';
+import React from 'react';
+import styled from 'styled-components';
+import Announcement from '../components/Announcement';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import NewsLetter from '../components/Newsletter';
+
+const Container = styled.div`
+    
+`
+const Wrapper = styled.div`
+    padding: 20px;
+    display: flex;
+`
+const ImgContainer = styled.div`
+    flex: 1;
+`
+const Image = styled.img`
+    width: 100%;
+    height: 90vh;
+    object-fit: cover;
+`
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 0px 50px;
+`
+const Title = styled.h1`
+    font-weight: 200;
+`
+const Description = styled.p`
+    margin: 20px 0px ;
+`
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 40px;
+`
+const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+`
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`
+const FilterColor = styled.p`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+    margin: 0px 5px;
+    cursor: pointer;
+`
+const FilterSize = styled.select`
+    padding: 5px;
+    margin-left: 10px;
+`
+const FilterSizeOption = styled.option`
+    
+`
+const AddContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 50%;
+`
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+`
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`
+const Button = styled.button`
+    padding: 15px;
+    border: 2px solid teal;
+    background-color: #fff;
+    cursor: pointer;
+    font-weight: 500;
+
+    &:hover{
+        background-color: #e7e5e5;
+    }
+`
+
+export default function Product(){
+
+    return(
+        <Container>
+            <Navbar />
+            <Announcement />
+            <Wrapper>
+                <ImgContainer>
+                    <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>Denim Jumpsuit</Title>
+                    <Description>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum maxime nisi magni! Error necessitatibus hic sit officiis facilis. Corporis, ex consectetur iste veniam provident voluptatem aut ducimus sunt nulla magnam?</Description>
+                    <Price>$ 20</Price>
+                    <FilterContainer>
+                        <Filter>
+                            <FilterTitle>Color</FilterTitle>
+                            <FilterColor color="black" />
+                            <FilterColor color="darkblue" />
+                            <FilterColor color="gray" />
+                        </Filter>
+                        <Filter>
+                            <FilterTitle>Size</FilterTitle>
+                            <FilterSize>
+                                <FilterSizeOption>XS</FilterSizeOption>
+                                <FilterSizeOption>S</FilterSizeOption>
+                                <FilterSizeOption>M</FilterSizeOption>
+                                <FilterSizeOption>L</FilterSizeOption>
+                                <FilterSizeOption>XL</FilterSizeOption>
+                            </FilterSize>
+                        </Filter>
+                    </FilterContainer>
+                    <AddContainer>
+                            <AmountContainer>
+                                <Remove />
+                                <Amount>1</Amount>
+                                <Add />
+                            </AmountContainer>
+                            <Button>ADD TO CART</Button>
+                        </AddContainer>
+                </InfoContainer>
+            </Wrapper>
+            <NewsLetter />
+            <Footer />
+        </Container>
+    )
+}
