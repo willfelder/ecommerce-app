6b5ded6b3aa8cdcfb91732219f7994d7d2789718
William Lengenfelder <william.lengenfelder@gmail.com>
2022-01-13 10:21:54 +0300

    deploy

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -109,7 +109,7 @@ function Slider(){
         if(direction === "left"){
             setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
         } else {
-            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+            setSlideIndex(slideIndex < 1 ? slideIndex + 1 : 0)
         }
     }
     return(
--- src/data.js
+++ src/data.js
@@ -6,15 +6,8 @@ export const SliderItems = [
         description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         backgroundColor: "f5fafd"
     },
-    {  
-        id: 2,
-        image: "https://i.ibb.co/cgCvVrr/girl1-png.png",
-        title: "30% OFF SALE",
-        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        backgroundColor: "fcf1ed"
-    },
     {
-        id: 3,
+        id: 2,
         image: "https://i.ibb.co/cXFnLLV/3.png",
         title: "SPRING SALE",
         description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
@@ -55,7 +48,7 @@ export const popularProducts = [
     },
     {
       id:4,
-      image:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+      image:"https://www.pngmart.com/files/4/Floral-Dress-PNG-Transparent-Image.png",
     },
     {
       id:5,
